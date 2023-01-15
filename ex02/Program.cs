3b8b0f3e494245395884ec119615b5c0128a86b2
AndreySh <shingaryovan@gmail.com>
2023-01-15 23:59:14 +1200

    добавлено второе упражнение - проверка четверти по введенным координатам X и Y

---
+++ ex02/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите X не равное 0:");
+int X = int.Parse(Console.ReadLine());
+while (X == 0)
+{
+    Console.WriteLine("Вы ввели 0, попробуйте ещё раз:");
+    X = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Введите Y не равное 0:");
+int Y = int.Parse(Console.ReadLine());
+while (Y == 0)
+{
+    Console.WriteLine("Вы ввели 0, попробуйте ещё раз:");
+    Y = int.Parse(Console.ReadLine());
+}
+if (X > 0 && Y > 0)
+{
+   Console.WriteLine("Первая четверть"); 
+}
+else if (X > 0 & Y < 0)
+{
+   Console.WriteLine("Четвертая четверть"); 
+}
+else if (X < 0 & Y > 0)
+{
+   Console.WriteLine("Вторая четверть"); 
+}
+else
+{
+   Console.WriteLine("Третья четверть"); 
+}
