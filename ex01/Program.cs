1758cb661a509793607890bf7a7cb4955b42c280
AndreySh <shingaryovan@gmail.com>
2023-01-15 23:23:30 +1200

    добавлено первое упражнение - проверка кратности введного числа 7ми и 23м

---
+++ ex01/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число для проверки кратности 7 и 23");
+int num = int.Parse(Console.ReadLine());
+if (num % 7 == 0 && num % 23 == 0)
+{
+    Console.WriteLine("Число " + num + " кратно 7 и 23");
+}
+else 
+{
+    Console.WriteLine("Число " + num + " Не кратно 7 и 23");
+}
